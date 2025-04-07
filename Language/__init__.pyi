532d59fab0183fbc8235823fd8d14f88760602fc
KM_CM <send.nex@gmail.com>
2025-04-07 21:21:28 +0300

    Add files via upload

---
+++ Language/__init__.pyi
@@ -0,0 +1,40 @@
+__LANGUAGE__: dict[ str, dict[ str, str ] ] = ...
+"""Stores all known ``Group``'s of translations. Each group is the ``Identifier`` and the ``Translation``."""
+
+def Add( Identifier: str, Translation: str, Group: str = ... ) -> None:
+    """
+``Add``'s an ``Identifier`` for ``Translation``.
+    """
+
+def AddGroup( Translations: dict[ str, str ], Group: str = ... ) -> None: ...
+
+def Get( Identifier: str, Group: str = ... ) -> str:
+    """
+``Get``'s the phrase at ``Identifier``.
+Returns ``None`` if not found.
+(For oh-no-my-editor-is-crying purposes,
+the ``| None`` part won't be annotated)
+    """
+
+def Free( Identifier: str, Group: str = ... ) -> None:
+    """
+``Free``'s the space at ``Identifier``.
+    """
+
+def FreeGroup( Group: str ) -> None: ...
+
+def Localize( String: str, Group: str = ... ) -> str:
+    """
+Localize everything in ``String``.
+Follows the format used by the Source Engine.
+
+For example, imagine that ``Add( 'EX', 'Example' )`` was run.
+'#EX' -> 'Example'
+'\\\\#EX' -> '#EX'
+'\\\\\\\\#EX -> '\\\\Example'
+
+Keys that aren't found are ignored.
+'#X' -> 'X'
+'\\#X' -> '#X'
+'\\\\#X' -> '\\X'
+    """
