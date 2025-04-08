9bc47393129b0252b2fb27eb2b7bd1c791eba0b7
KM_CM <send.nex@gmail.com>
2025-04-08 17:38:51 +0300

    Update __init__.pyi

--- KM_CM_Language/__init__.pyi
+++ KM_CM_Language/__init__.pyi
@@ -7,7 +7,7 @@ The ``Language`` library will help you with localizing strings.
 
 Directly modyfing the ``__LANGUAGE__`` dictionary is not recommended nor is it safe.
 
-Avaible translation identifier characters.
+Identifiers for translations and groups can only contain the following set of characters.
 { '_',
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
